d3f3a725ca41e3ae0aee9a11e1f217b46d35fbff
joegelay <joegelay@gmail.com>
2020-03-30 15:01:49 -0600

    refactor code for kid validations

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,7 +1,4 @@
 class Kid < ApplicationRecord
-    validates :username, presence: true 
-    validates :age, presence: true 
-    validates :age, numericality: { greater_than: 6 }
-    validates :age, numericality: { less_than: 15 }
-    valudates :username, length: { minimum: 7, maximum: 25 }
+    validates :username, presence: true, length: { minimum: 7, maximum: 25 }
+    validates :age, presence: true, numericality: { greater_than: 6, less_than: 15 }
 end
